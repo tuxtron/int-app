c4556844c8abce027f07444ca7c89cb0477cb5fb
Kevin Yao <vasty85817@gmail.com>
2021-10-01 21:22:11 -0300

    first push

--- src/store/reducers/movies.js
+++ src/store/reducers/movies.js
@@ -78,120 +78,120 @@ const initialState = {
     ],
     categories: [
         {
-          "_id": "randomid",
-          "name": "Terror",
-          "pos": 0,
-          "movies": [
-            {
-              "pos": 0,
-              "movie": {
-                "_id": "randomid",
-                "title": "The Conjuring",
-                "duration": "2h",
-                "value": "9/10",
-                "description": "Película de Terror",
-                "minAge": "18",
-                "launch": "09/11/2016 16:16pm (GMT)",
-                "image": "https://images.alphacoders.com/822/822222.jpg",
-                "imageMobile": "https://pelisporlanoche.files.wordpress.com/2013/10/cb1d3-elconjurocaratula.jpg?w=640",
-                "images": [
-                  "https://images.alphacoders.com/822/822222.jpg",
-                  "https://www.xtrafondos.com/wallpapers/resized/el-conjuro-726.jpg?s=large"
-                ],
-                "movieUrl": "https://vod-progressive.akamaized.net/exp=1633124879~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4924%2F7%2F199620587%2F673437355.mp4~hmac=2e9e52f9607a20af2852d6d07936139489e6f7ca3caee804b15cc6ff736e714c/vimeo-prod-skyfire-std-us/01/4924/7/199620587/673437355.mp4?filename=The+Conjuring+-+International+Trailer+%2872404%29.mp4",
-                "characters": [
-                  "Ed Warren",
-                  "Lorraine Warren"
-                ],
-                "director": "James Wan",
-                "producer": "Tony DeRosa-Grund"
+            "_id": "randomid",
+            "name": "Terror",
+            "pos": 0,
+            "movies": [
+              {
+                "pos": 0,
+                "movie": {
+                  "_id": "randomid",
+                  "title": "The Conjuring",
+                  "duration": "2h",
+                  "value": "9/10",
+                  "description": "Película de Terror",
+                  "minAge": "18",
+                  "launch": "09/11/2016 16:16pm (GMT)",
+                  "image": "https://images.alphacoders.com/822/822222.jpg",
+                  "imageMobile": "https://pelisporlanoche.files.wordpress.com/2013/10/cb1d3-elconjurocaratula.jpg?w=640",
+                  "images": [
+                    "https://images.alphacoders.com/822/822222.jpg",
+                    "https://www.xtrafondos.com/wallpapers/resized/el-conjuro-726.jpg?s=large"
+                  ],
+                  "movieUrl": "https://es.vid.web.acsta.net/nmedia/34/19/06/03/14//19562331_hd_013.mp4",
+                  "characters": [
+                    "Ed Warren",
+                    "Lorraine Warren"
+                  ],
+                  "director": "James Wan",
+                  "producer": "Tony DeRosa-Grund"
+                }
+              },
+              {
+                "pos": 1,
+                "movie": {
+                  "_id": "randomid",
+                  "title": "Insidious",
+                  "duration": "3h",
+                  "value": "9/10",
+                  "description": "Película de terror muy de terror",
+                  "minAge": "22",
+                  "launch": "09/11/2016 16:16pm (GMT)",
+                  "image": "https://images6.alphacoders.com/804/804808.jpg",
+                  "imageMobile": "https://vistapointe.net/images/insidious-wallpaper-8.jpg",
+                  "images": [
+                    "https://images6.alphacoders.com/804/804808.jpg",
+                    "https://vistapointe.net/images/insidious-7.jpg"
+                  ],
+                  "movieUrl": "https://es.vid.web.acsta.net/nmedia/34/19/06/03/14//19562331_hd_013.mp4",
+                  "characters": [
+                    "Josh Lambert",
+                    "Elise Rainier"
+                  ],
+                  "director": "Director",
+                  "producer": "Producer"
+                }
               }
-            },
-            {
-              "pos": 1,
-              "movie": {
-                "_id": "randomid",
-                "title": "Insidious",
-                "duration": "3h",
-                "value": "9/10",
-                "description": "Película de terror muy de terror",
-                "minAge": "22",
-                "launch": "09/11/2016 16:16pm (GMT)",
-                "image": "https://images6.alphacoders.com/804/804808.jpg",
-                "imageMobile": "https://vistapointe.net/images/insidious-wallpaper-8.jpg",
-                "images": [
-                  "https://images6.alphacoders.com/804/804808.jpg",
-                  "https://vistapointe.net/images/insidious-7.jpg"
-                ],
-                "movieUrl": "https://vod-progressive.akamaized.net/exp=1633124879~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4924%2F7%2F199620587%2F673437355.mp4~hmac=2e9e52f9607a20af2852d6d07936139489e6f7ca3caee804b15cc6ff736e714c/vimeo-prod-skyfire-std-us/01/4924/7/199620587/673437355.mp4?filename=The+Conjuring+-+International+Trailer+%2872404%29.mp4",
-                "characters": [
-                  "Josh Lambert",
-                  "Elise Rainier"
-                ],
-                "director": "Director",
-                "producer": "Producer"
+            ]
+          },
+          {
+            "_id": "randomid",
+            "name": "Drama",
+            "pos": 1,
+            "movies": [
+              {
+                "pos": 0,
+                "movie": {
+                  "_id": "randomid",
+                  "title": "Seven Pounds",
+                  "duration": "1h30min",
+                  "value": "9/10",
+                  "description": "Drama",
+                  "minAge": "15",
+                  "launch": "09/11/2016 16:16pm (GMT)",
+                  "image": "https://www.funeralnatural.net/sites/default/files/pelicula/imagen/sietealmascartel.jpg",
+                  "imageMobile": "https://i.blogs.es/5c56b8/seven_pounds/450_1000.jpg",
+                  "images": [
+                    "https://www.funeralnatural.net/sites/default/files/pelicula/imagen/sietealmascartel.jpg",
+                    "https://pics.filmaffinity.com/Siete_almas-151960634-large.jpg"
+                  ],
+                  "movieUrl": "https://es.vid.web.acsta.net/nmedia/34/19/06/03/14//19562331_hd_013.mp4",
+                  "characters": [
+                    "Ben/Tim Thomas",
+                    "Emily Posa"
+                  ],
+                  "director": "Gabriele Muccino",
+                  "producer": "Todd Black"
+                }
+              },
+              {
+                "pos": 1,
+                "movie": {
+                  "_id": "randomid",
+                  "title": "Forrest Gump",
+                  "duration": "2h10min",
+                  "value": "9/10",
+                  "description": "Película de drama",
+                  "minAge": "16",
+                  "launch": "09/11/2016 16:16pm (GMT)",
+                  "image": "https://wallpaperaccess.com/full/1455905.jpg",
+                  "imageMobile": "https://i.pinimg.com/564x/dc/f1/13/dcf11341e68fa8a58a849636ae500c2c.jpg",
+                  "images": [
+                    "https://wallpaperaccess.com/full/1455905.jpg",
+                    "https://media.gettyimages.com/photos/forrest-gump-1994-directed-by-robert-zemeckis-picture-id932243246?s=612x612"
+                  ],
+                  "movieUrl": "https://es.vid.web.acsta.net/nmedia/34/19/06/03/14//19562331_hd_013.mp4",
+                  "characters": [
+                    "Forrest Gump",
+                    "Jenny Curran"
+                  ],
+                  "director": "Robert Zemeckis",
+                  "producer": "Wendy Finerman"
+                }
               }
-            }
-          ]
-        },
-        {
-          "_id": "randomid",
-          "name": "Drama",
-          "pos": 1,
-          "movies": [
-            {
-              "pos": 0,
-              "movie": {
-                "_id": "randomid",
-                "title": "Seven Pounds",
-                "duration": "1h30min",
-                "value": "9/10",
-                "description": "Drama",
-                "minAge": "15",
-                "launch": "09/11/2016 16:16pm (GMT)",
-                "image": "https://www.funeralnatural.net/sites/default/files/pelicula/imagen/sietealmascartel.jpg",
-                "imageMobile": "https://pics.filmaffinity.com/Siete_almas-243841583-large.jpg",
-                "images": [
-                  "https://www.funeralnatural.net/sites/default/files/pelicula/imagen/sietealmascartel.jpg",
-                  "https://pics.filmaffinity.com/Siete_almas-151960634-large.jpg"
-                ],
-                "movieUrl": "https://vod-progressive.akamaized.net/exp=1633124879~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4924%2F7%2F199620587%2F673437355.mp4~hmac=2e9e52f9607a20af2852d6d07936139489e6f7ca3caee804b15cc6ff736e714c/vimeo-prod-skyfire-std-us/01/4924/7/199620587/673437355.mp4?filename=The+Conjuring+-+International+Trailer+%2872404%29.mp4",
-                "characters": [
-                  "Ben/Tim Thomas",
-                  "Emily Posa"
-                ],
-                "director": "Gabriele Muccino",
-                "producer": "Todd Black"
-              }
-            },
-            {
-              "pos": 1,
-              "movie": {
-                "_id": "randomid",
-                "title": "Forrest Gump",
-                "duration": "2h10min",
-                "value": "9/10",
-                "description": "Película de drama",
-                "minAge": "16",
-                "launch": "09/11/2016 16:16pm (GMT)",
-                "image": "https://wallpaperaccess.com/full/1455905.jpg",
-                "imageMobile": "https://i.pinimg.com/564x/dc/f1/13/dcf11341e68fa8a58a849636ae500c2c.jpg",
-                "images": [
-                  "https://wallpaperaccess.com/full/1455905.jpg",
-                  "https://media.gettyimages.com/photos/forrest-gump-1994-directed-by-robert-zemeckis-picture-id932243246?s=612x612"
-                ],
-                "movieUrl": "https://vod-progressive.akamaized.net/exp=1633125080~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4679%2F7%2F1983%E2%80%A6",
-                "characters": [
-                  "Forrest Gump",
-                  "Jenny Curran"
-                ],
-                "director": "Robert Zemeckis",
-                "producer": "Wendy Finerman"
-              }
-            }
-          ]
-        }
-      ],
+            ]
+          }
+    ],
     allMovies: [
         {
             "pos": 0,
